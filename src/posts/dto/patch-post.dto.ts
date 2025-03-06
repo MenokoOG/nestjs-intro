856f750c1f03340bd44a749454a624f9855627ef
Lawrence Jefferson II <jefftkddan@gmail.com>
2025-03-06 13:31:56 -0800

    lesson updates

---
+++ src/posts/dto/patch-post.dto.ts
@@ -0,0 +1,13 @@
+import { CreatePostDto } from './create-post.dto';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { IsInt, IsNotEmpty } from 'class-validator';
+
+export class PatchPostDto extends PartialType(CreatePostDto) {
+  @ApiProperty({
+    description: 'The ID of the post to update.',
+    example: 1,
+  })
+  @IsInt()
+  @IsNotEmpty()
+  id: number;
+}
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,28 +1,39 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Patch } from '@nestjs/common';
 import { PostsService } from './providers/posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { PatchPostDto } from './dto/patch-post.dto';
+import { ApiOperation, ApiResponse, ApiTags, ApiParam } from '@nestjs/swagger';
 
 @Controller('posts')
 @ApiTags('Posts')
 export class PostsController {
   constructor(
     /*
-     *  Injecting Posts Service
+     * Injecting Posts Service
      */
     private readonly postsService: PostsService,
   ) {}
 
-  /*
-   * GET localhost:3000/posts/:userId
-   */
   @Get('/:userId')
+  @ApiOperation({
+    summary: 'Retrieve all posts for a specific user',
+  })
+  @ApiParam({
+    name: 'userId',
+    type: Number,
+    description: 'ID of the user whose posts are being retrieved',
+    example: 123,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully retrieved the posts for the given user.',
+  })
   public getPosts(@Param('userId') userId: number) {
     return this.postsService.findAll(userId);
   }
 
   @ApiOperation({
-    summary: 'Creates a new post for the blog.',
+    summary: 'Creates a new post for the blog',
   })
   @ApiResponse({
     status: 201,
@@ -32,5 +43,18 @@ export class PostsController {
   @Post()
   public createPost(@Body() createPostDto: CreatePostDto) {
     console.log(createPostDto);
+  }
+
+  @ApiOperation({
+    summary: 'Updates an existing blog post in the database',
+  })
+  @ApiResponse({
+    status: 200,
+    description:
+      'You get a success 200 response if the post is updated successfully',
+  })
+  @Patch()
+  public updatePost(@Body() patchPostsDto: PatchPostDto) {
+    console.log(patchPostsDto);
   }
 }
